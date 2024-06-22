9691a01a9fc9933fbce6ad6972093662f2e2182e
Sarocha <rocha.right007@gmail.com>
2024-06-22 15:12:52 -0600

    completed code

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -23,13 +23,35 @@ public class CashRegister {
 		if(money == price) {
 			System.out.println("Receive exact amount, Thank you");
 		}
+		else if (money > price){
+		float AmountChange = (float) (money - price);
+		System.out.println(AmountChange);
 		
-		double AmountChange = money - price;
+		int bill_20 = 0;
+		int bill_10 = 0;
+		int bill_5 = 0;
+		int bill_1 = 0;
 		int quarter = 0;
 		int dime = 0;
 		int nickel = 0;
 		int penny = 0;
 		
+		while(AmountChange >= 20) {
+			AmountChange -= 20;
+			bill_20 ++;
+		}
+		while(AmountChange >= 10) {
+			AmountChange -= 10;
+			bill_10 ++;
+		}
+		while(AmountChange >= 5) {
+			AmountChange -= 5;
+			bill_5 ++;
+		}
+		while(AmountChange >= 1) {
+			AmountChange -= 1;
+			bill_1 ++;
+		}
 		while(AmountChange >= 0.25) {
 			AmountChange -= 0.25;
 			quarter ++;
@@ -47,8 +69,77 @@ public class CashRegister {
 			penny++;
 		}
 		
+		System.out.print("Change: ");
+		
+		if (bill_20 == 1) {
+			System.out.print(bill_20 + " twenty dollar bill ");
+		}
+		
+		if (bill_20 > 1) {
+			System.out.print(bill_20 + " twenty dollar bills ");
+		}
+		
+		if (bill_10 == 1) {
+			System.out.print(bill_10 + " ten dollar bill ");
+		}
+		
+		if (bill_10 > 1) {
+			System.out.print(bill_10 + " ten dollar bills ");
+		}
+		
+		if (bill_5 == 1) {
+			System.out.print(bill_5 + " five dollar bill ");
+		}
+		
+		if (bill_5 > 1) {
+			System.out.print(bill_5 + " five dollar bills ");
+		}
+		
+		if (bill_1 == 1) {
+			System.out.print(bill_1 + " one dollar bill ");
+		}
+		
+		if (bill_1 > 1) {
+			System.out.print(bill_1 + " one dollar bill ");
+		}
+		
+		if (quarter == 1) {
+			System.out.print(quarter + " quarter ");
+		}
+		
+		if (quarter > 1) {
+			System.out.print(quarter + " quarters ");
+		}
+		
+		if (dime == 1) {
+			System.out.print(dime + " dime ");
+		}
+		
+		if (dime > 1) {
+			System.out.print(dime + " dimes ");
+		}
+		
+		if (nickel == 1) {
+			System.out.print(nickel + " nickel ");
+		}
+		
+		if (nickel > 1) {
+			System.out.print(nickel + " nickels ");
+		}
+		
+		if (penny == 1) {
+			System.out.print(penny + " penny ");
+		}
+		
+		if (penny >= 1) {
+			System.out.print(penny + " pennies ");
+		}
+		
+		}
 		
 		keyboard.close();
+		
+
 	}
 	
 }
